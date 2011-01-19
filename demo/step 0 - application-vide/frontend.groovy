83cb0588b48e4c24e9785d5377943b55e2aa6505
Maxime Hamm <mhamm@jspresso.org>
2011-01-19 15:30:21 +0000

    cleaning before recreating new project

--- demo/step 0 - application-vide/frontend.groovy
+++
@@ -1,7 +0,0 @@
-// Implement your application frontend here using the SJS DSL.
-controller ('directory.controller',
-    icon:'directory.png',
-    context:'directory',
-    language:'fr',
-    workspaces:[/* Enter your workspaces here */])
-
--- demo/step 0 - application-vide/model.groovy
+++
@@ -1,2 +0,0 @@
-// Implement your domain here using the SJS DSL.
-   
