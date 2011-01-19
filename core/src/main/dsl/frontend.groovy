1c89af2e2528f2c4fa8ec43f4a8b37b1acd45cdd
Maxime Hamm <mhamm@jspresso.org>
2011-01-19 20:51:01 +0000

    evolution new directory project

---
+++ core/src/main/dsl/frontend.groovy
@@ -0,0 +1,12 @@
+// Implement your application frontend here using the SJS DSL.
+
+/*
+ * The workspaces and modules
+ */
+
+
+controller 'directory.name',
+  icon:'icon.png',
+  context:'directory',
+  language:'en',
+  workspaces:[/*Reference your workspaces here.*/]
