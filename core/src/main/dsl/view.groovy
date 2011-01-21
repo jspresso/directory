c35208b8461217ea90b8c1cac53ab1388a31bbc8
Vincent Vandenschrick <vvandens@jspresso.org>
2011-01-21 07:45:41 +0000

    Fixed category module collection view

--- core/src/main/dsl/view.groovy
+++ core/src/main/dsl/view.groovy
@@ -1,55 +1,50 @@
 // Implement your application view here using the SJS DSL.
-border('Customer.view', 
-    actionMap:'beanModuleActionMap') {
+border('Customer.view',
+  actionMap:'beanModuleActionMap') {
+    north { form (model:'Customer') }
+    center {
+      tabs (renderingOptions:'LABEL') {
+        views {
+          border (cascadingModels:true) {
+            north {
+              table (model:'Customer-contacts', actionMap:'masterDetailActionMap')
+            }
+            center { border (parent:'Contact.readonly.view') }
+          }
+
+          table (model:'Customer-addresses', actionMap:'masterDetailActionMap', )
+        }
+      }
+    }
+  }
+
+border ('Contact.view', parent:'Contact.readonly.view',  actionMap:'beanModuleActionMap')
+
+border ('Contact.readonly.view', borderType:'TITLED') {
   north {
-    form (model:'Customer')
+    form (model:'Contact', columnCount:2, fields:['lastname', 'firstname'])
   }
   center {
     tabs (renderingOptions:'LABEL') {
+
       views {
-        border (cascadingModels:true) {
-          north {
-            table (model:'Customer-contacts', actionMap:'masterDetailActionMap')
-          }
-          center {
-            border (parent:'Contact.readonly.view')
-          }
-        }
-        
-        table (model:'Customer-addresses', actionMap:'masterDetailActionMap', )
-        
-      }   
+        table (model:'Contact-phoneNumbers',
+          columns:['type', 'number'],
+          actionMap:'masterDetailActionMap')
+
+        form (model:'Contact',
+          fields:['comments'], labelsPosition:'NONE', name:'comments')
+
+        table (model:'Contact-activities',
+          actionMap:'masterDetailActionMap')
+      }
     }
   }
 }
 
-border ('Contact.view', parent:'Contact.readonly.view',  actionMap:'beanModuleActionMap')    
-    
-border ('Contact.readonly.view', borderType:'TITLED') {
-      north {
-        form (model:'Contact', columnCount:2, fields:['lastname', 'firstname'])
-      }
-      center {
-        tabs (renderingOptions:'LABEL') {
-          
-          views {
-            table (model:'Contact-phoneNumbers',
-                columns:['type', 'number'],
-                actionMap:'masterDetailActionMap')
-            
-            form (model:'Contact', 
-                fields:['comments'], labelsPosition:'NONE', name:'comments')
-            
-            table (model:'Contact-activities',
-                actionMap:'masterDetailActionMap')
-          }
-        }
-      }
-    }
-
 split_vertical('Category.view', cascadingModels:true) {
   top {
-    table (validationModel:'Category', parent:'decoratedView', actionMap:'filterableBeanCollectionModuleActionMap') 
+    table (validationModel:'Category', parent:'decoratedView', actionMap:'filterableBeanCollectionModuleActionMap')
   }
   bottom {
     split_horizontal(right:'Category.tree') {
@@ -58,30 +53,29 @@ split_vertical('Category.view', cascadingModels:true) {
       }
     }
   }
-}   
-
-tree ('Category.tree', rendered:'categoryname') {
-  subTree ('Category-subCategories.treenode')
 }
 
-treeNode('Category-subCategories.treenode', 
+tree ('Category.tree', rendered:'categoryname') { subTree ('Category-subCategories.treenode') }
+
+treeNode('Category-subCategories.treenode',
   rendered:'categoryname')
 
-table ('PhoneNumber.table', 
-    parent:'decoratedView', readOnly:true) { 
-      actionMap {
-        actionList('QUERY') { 
-          action ref:'queryModuleFilterAction'
-        }
-      }
+table ('PhoneNumber.table',
+  parent:'decoratedView', readOnly:true) {
+    actionMap {
+      actionList('QUERY') {  action ref:'queryModuleFilterAction' }
     }
-    
+  }
+
 border ('CategoryModule.statistics.view', cascadingModels:true, validationModel:'Category') {
   west {
-    table (model:'Category', columns:['categoryname'])
+    table (columns:['categoryname'])
   }
   center {
-    table (model:'Category-contacts', columns:['customer.customername', 'lastname', 'firstname'])
-    
+    table (model:'Category-contacts', columns:[
+      'customer.customername',
+      'lastname',
+      'firstname'
+    ])
   }
-}    
+}
