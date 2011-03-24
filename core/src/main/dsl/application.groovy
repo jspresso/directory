b7dc3e0c0b7ab04c58170cafc98641a554302dd2
Maxime Hamm <mhamm@jspresso.org>
2011-03-24 21:17:49 +0000

    evolution auto build

--- core/src/main/dsl/application.groovy
+++ core/src/main/dsl/application.groovy
@@ -3,22 +3,25 @@ import org.jspresso.contrib.sjs.front.Front;
 
 def domainBuilder = new Domain()
 
+String errorOutput = project.properties['outputDir']+"/../../sjsErrors.log";
+
 domainBuilder.Domain(projectName:'directory', mute:true) {
-  namespace('com.example.directory') {
+  namespace('fr.gefco.directory') {
     include(project.properties['srcDir']+'/model.groovy')
   }
 }
 if(!domainBuilder.isOK()) {
   println domainBuilder.getErrorDomain()
-  fail('SJS defined domain is invalid.')
+  new File(errorOutput).write('SJS defined domain is invalid :\n' + domainBuilder.getErrorDomain())
+  fail('SJS defined domain is invalid :\n' + domainBuilder.getErrorDomain())
 }
 
 def frontendBuilder = new Front(domainBuilder.getReferenceDomain())
 frontendBuilder.Front(){
-  namespace('com.example.directory'){
+  namespace('fr.gefco.directory'){
     view {
       include(project.properties['srcDir']+'/view.groovy')
-      include(project.properties['srcDir']+'/viewTools.groovy')
+	  include(project.properties['srcDir']+'/viewTools.groovy')
     }
     frontend {
       include(project.properties['srcDir']+'/frontend.groovy')
@@ -26,14 +29,20 @@ frontendBuilder.Front(){
     backend {
       include(project.properties['srcDir']+'/backend.groovy')
     }
-  }
+  } 
 }
 if(frontendBuilder.getNbrError() != 0) {
   println frontendBuilder.getError()
-  fail('SJS defined frontend / views is invalid.')
+  new File(errorOutput).write('SJS defined frontend / views is invalid :\n' + frontendBuilder.getErrorDomain())
+  fail('SJS defined frontend / views is invalid :\n' + frontendBuilder.getErrorDomain())
 }
 
-domainBuilder.writeDomainFile(project.properties['outputDir'],project.properties['modelOutputFileName'])
+// write files
+domainBuilder.writeDomainFile(project.properties['outputDir'], project.properties['modelOutputFileName'])
 frontendBuilder.writeOutputFile('backend',project.properties['outputDir'],project.properties['backOutputFileName'])
 frontendBuilder.writeOutputFile('view',project.properties['outputDir'],project.properties['viewOutputFileName'])
 frontendBuilder.writeOutputFile('frontend',project.properties['outputDir'],project.properties['frontOutputFileName'])
+
+//
+new File(errorOutput).write("")
+println "SJS Finished"
